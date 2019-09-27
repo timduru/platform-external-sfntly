msft-mirror-aosp/platform.external.sfntly

Fix uninitialized value in sfntly
Bug: 116114182
Test: build
Change-Id: Ief84dd8a19bdb461945e07504270be76a3545701

cpp/src/sfntly/port/file_input_stream.cc
@@ -74,7 +74,7 @@ int32_t FileInputStream::Read() {
 #endif
     return 0;
   }
-  uint8_t value;
+  uint8_t value = 0;
   size_t length = fread(&value, 1, 1, file_);
   position_ += length;
   return value;

timduru/platform-external-sfntly

Merged-In: Ief84dd8a19bdb461945e07504270be76a3545701
(cherry picked from commit 51ecc82faa63aacdde6648f80a3898b53653da78)

cpp/src/sfntly/port/file_input_stream.cc
@@ -70,7 +70,7 @@ int32_t FileInputStream::Read() {
 #endif
     return 0;
   }
-  byte_t value;
+  byte_t value = 0;
   size_t length = fread(&value, 1, 1, file_);
   position_ += length;
   return value;